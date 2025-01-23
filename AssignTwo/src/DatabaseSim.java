fb0fd868bc7cade344b6f3eea926bc4bdf0d1b73
Michaels Lumanga <115460525+6a6aQth@users.noreply.github.com>
2025-01-23 16:05:20 +0200

    Delete AssignTwo directory

--- AssignTwo/src/DatabaseSim.java
+++
@@ -1,7 +0,0 @@
-// Interface for simulating database lookup
-public interface DatabaseSim {
-    // Method to check if username and password match
-    boolean authenticate(String id, String password);
-
-}
-
